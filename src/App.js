0bd31c55faf28fb3fb5f2429082aa66ff677b114
ay-sh <asharma.bhwm@gmail.com>
2021-12-23 02:41:02 +0530

    complete task with basic ui animation done (may need rework)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import moment from 'moment';
 import './App.css';
 
 import Task from './components/Task';
 
+
+/*
+  status
+    pending
+    done
+*/
 function App() {
+  
+  const statusEnum = {
+    STATUS_PENDING: false,
+    STATUS_DONE: true
+  }
+
+  const sampleTasks = [{
+    title: "Go for a walk",
+    createdAt: moment().format('MMM Do YYYY'),
+    status: statusEnum.STATUS_PENDING,
+  },{
+    title: "Do other work",
+    createdAt: moment().add(2,'d').format('MMM Do YYYY'),
+    status: statusEnum.STATUS_DONE
+  }] 
+
   return (
     <div className="App">
-      <Task />
-      <Task />
+      <Task taskData={sampleTasks[0]}/>
+      <Task taskData={sampleTasks[1]}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import './Task.css'
 
-const Task = () => {
+const Task = (props) => {
+    const {title, createdAt, status} = props.taskData;
+    const[localStatus, setLocalStatus] = useState(status);
+    
+    const taskFinishHandler = () => {
+        setLocalStatus(prevLocalStatus => !prevLocalStatus);
+    }
+
     return (
         <div className='taskContainer'>
-            <button className='taskContainer__finishbtn'>P</button>
-            <button className='taskContainer__finishbtn  translateLeft__16--modifier'>D</button>
+            <button className='taskContainer__btn normalbtnStyle'>P</button>
+            <button className= {`taskContainer__btn translateLeft__16--modifier ${localStatus ? 'finishbtnStyle':'normalbtnStyle'}`} 
+                    
+                    onClick={taskFinishHandler}>
+                        D
+            </button>
             <div className='taskContainer__taskdatacontainer'>
-                <p className="taskContainer__title">Implement task sorting</p>
-                <p className="taskContainer__metadata">created at: 12/22/2021</p>
+                <p className="taskContainer__title">
+                    <span className={`titleStrikethrough ${localStatus ? 'titleStrikethroughDone':''}`}>{title}</span>
+                </p>
+                <p className="taskContainer__metadata">created at: {createdAt}</p>
             </div>
         </div>
     )
